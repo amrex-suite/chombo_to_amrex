6378cfc51b9bc78d134d1254da5023ec91041e7a
nataraj2 <mn556@cornell.edu>
2026-09-03 15:58:12 -0700

    Adding CNS folder

---
+++ CNS/Exec/Pulse/tmp_build_dir/o/3d.gnu.x86-milan.DEBUG.EXE/AMReX_GpuControl.d
@@ -0,0 +1,10 @@
+tmp_build_dir/o/3d.gnu.x86-milan.DEBUG.EXE/AMReX_GpuControl.o: \
+ ../../../amrex/Src/Base/AMReX_GpuControl.cpp \
+ ../../../amrex/Src/Base/AMReX_GpuControl.H \
+ tmp_build_dir/s/3d.gnu.x86-milan.DEBUG.EXE/AMReX_Config.H \
+ ../../../amrex/Src/Base/AMReX_GpuQualifiers.H \
+ ../../../amrex/Src/Base/AMReX_GpuTypes.H
+../../../amrex/Src/Base/AMReX_GpuControl.H:
+tmp_build_dir/s/3d.gnu.x86-milan.DEBUG.EXE/AMReX_Config.H:
+../../../amrex/Src/Base/AMReX_GpuQualifiers.H:
+../../../amrex/Src/Base/AMReX_GpuTypes.H:
